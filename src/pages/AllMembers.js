5a04b0706f93e3cfa32b45a968242e057edca277
kpattar <kpattar@altimetrik.com>
2025-01-14 15:54:21 +0530

    refactor with fields in data file

--- src/pages/AllMembers.js
+++ src/pages/AllMembers.js
@@ -9,122 +9,7 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Profilepic from "../components/Profilepic.js";
 import api from "../api.js";
-
-const fields = [
-  { label: "Pulli ID (Primary Key)", name: "pulli_id", required: true },
-  { label: "Family Name", name: "family_name", required: true },
-  { label: "Name", name: "name", required: true },
-  { label: "Spouse Name", name: "spouse_name", required: false },
-  {
-    label: "Address Line 1",
-    name: "address_line_1",
-    required: true,
-    type: "text area",
-  },
-  { label: "Address Line 2", name: "addressLine2", required: true },
-  { label: "City", name: "city", required: true },
-  { label: "State", name: "state", required: true },
-  { label: "Pin Code", name: "pin_code", required: true },
-  { label: "Mobile Number 1", name: "mobile_1", required: true, type: "tel" },
-  {
-    label: "Mobile Number 2(Spouse)",
-    name: "mobile2(spouse)",
-    required: true,
-    type: "tel",
-  },
-  {
-    label: "Whatsapp Number 1",
-    name: "Whatsapp1",
-    required: true,
-    type: "tel",
-  },
-  {
-    label: "Whatsapp Number 2",
-    name: "Whatsapp2",
-    required: true,
-    type: "tel",
-  },
-  { label: "Email ID 1", name: "email_id_1", required: true, type: "email" },
-  {
-    label: "Native",
-    name: "native",
-    required: true,
-    type: "dropdown",
-    options: [
-      { value: "valayapatti", label: "valayapatti" },
-      { value: "Kallal", label: "Kallal" },
-      { value: "Kandanur", label: "Kandanur" },
-      { value: "Karaikudi", label: "Karaikudi" },
-      { value: "Melaisivapuri", label: "Melaisivapuri" },
-    ],
-  },
-  {
-    label: "Karai",
-    name: "karai",
-    required: true,
-    type: "dropdown",
-    options: [
-      { value: "Panaivaikum Karai", label: "Panaivaikum Karai" },
-      { value: "Samiyadi karai", label: "Samiyadi karai" },
-      { value: "Poosari karai", label: "Poosari karai" },
-    ],
-  },
-  {
-    label: "Token Number",
-    name: "token_number",
-    required: false,
-    type: "number",
-  },
-  {
-    label: "Token Year",
-    name: "token_year",
-    required: false,
-    type: "dropdown", // Specify that this is a dropdown field
-    options: [
-      { value: "2005", label: "2005" },
-      { value: "2006", label: "2006" },
-      { value: "2007", label: "2007" },
-      { value: "2008", label: "2008" },
-      { value: "2009", label: "2009" },
-      { value: "2010", label: "2010" },
-      { value: "2011", label: "2011" },
-      { value: "2012", label: "2012" },
-      { value: "2013", label: "2013" },
-      { value: "2014", label: "2014" },
-      { value: "2015", label: "2015" },
-      { value: "2016", label: "2016" },
-      { value: "2017", label: "2017" },
-      { value: "2018", label: "2018" },
-      { value: "2019", label: "2019" },
-      { value: "2020", label: "2020" },
-      { value: "2021", label: "2021" },
-      { value: "2022", label: "2022" },
-      { value: "2023", label: "2023" },
-      { value: "2024", label: "2024" },
-      { value: "2025", label: "2025" },
-      { value: "2026", label: "2026" },
-      { value: "2027", label: "2027" },
-      { value: "2028", label: "2028" },
-      { value: "2029", label: "2029" },
-      { value: "2030", label: "2030" },
-      { value: "2031", label: "2031" },
-      { value: "2032", label: "2032" },
-      { value: "2033", label: "2033" },
-      { value: "2034", label: "2034" },
-      { value: "2035", label: "2035" },
-      { value: "2036", label: "2036" },
-      { value: "2037", label: "2037" },
-      { value: "2038", label: "2038" },
-      { value: "2039", label: "2039" },
-      { value: "2040", label: "2040" },
-      { value: "2041", label: "2041" },
-      { value: "2042", label: "2042" },
-      { value: "2043", label: "2043" },
-      { value: "2044", label: "2044" },
-      { value: "2045", label: "2045" },
-    ],
-  },
-];
+import { editFormFields } from "../assets/Data.js";
 
 const AllMembers = () => {
   const [openEditModal, setOpenEditModal] = useState(false); // Modal visibility
@@ -214,7 +99,7 @@ const AllMembers = () => {
           <DialogTitle>Edit Member</DialogTitle>
           <DialogContent>
             <Profilepic />
-            {fields.map((field) => (
+            {editFormFields.map((field) => (
               <div key={field.name}>
                 {field.type === "dropdown" ? (
                   <FormControl fullWidth margin="normal">
--- src/pages/NewMember.js
+++ src/pages/NewMember.js
@@ -1,73 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import Form from "../components/Form";
-import { Grid, Box, Paper, Alert } from "@mui/material";
+import { Paper } from "@mui/material";
 import api from "../api";
 import { Navigate } from "react-router-dom";
+import { formFields } from "../assets/Data";
 
 function NewMember() {
-  const [loading, setLoading] = useState(false);
-
-  const fields = [
-    { label: "Pulli ID (Primary Key)", name: "pulli_id", required: true },
-    { label: "Family Name", name: "family_name", required: true },
-    { label: "Name", name: "name", required: true },
-    { label: "Spouse Name", name: "spouse_name", required: false },
-    {
-      label: "Address Line 1",
-      name: "address_line_1",
-      required: true,
-      type: "text area",
-    },
-    { label: "Address Line 2", name: "addressLine2", required: true },
-    { label: "City", name: "city", required: true },
-    { label: "State", name: "state", required: true },
-    { label: "Pin Code", name: "pin_code", required: true },
-    { label: "Mobile Number 1", name: "mobile_1", required: true, type: "tel" },
-    {
-      label: "Mobile Number 2(Spouse)",
-      name: "mobile2(spouse)",
-      required: true,
-      type: "tel",
-    },
-    {
-      label: "Whatsapp Number 1",
-      name: "Whatsapp1",
-      required: true,
-      type: "tel",
-    },
-    {
-      label: "Whatsapp Number 2",
-      name: "Whatsapp2",
-      required: true,
-      type: "tel",
-    },
-    { label: "Email ID 1", name: "email_id_1", required: true, type: "email" },
-    {
-      label: "Native",
-      name: "native",
-      required: true,
-      type: "dropdown",
-      options: [
-        { value: "valayapatti", label: "valayapatti" },
-        { value: "Kallal", label: "Kallal" },
-        { value: "Kandanur", label: "Kandanur" },
-        { value: "Karaikudi", label: "Karaikudi" },
-        { value: "Melaisivapuri", label: "Melaisivapuri" },
-      ],
-    },
-    {
-      label: "Karai",
-      name: "karai",
-      required: true,
-      type: "dropdown",
-      options: [
-        { value: "Panaivaikum Karai", label: "Panaivaikum Karai" },
-        { value: "Samiyadi karai", label: "Samiyadi karai" },
-        { value: "Poosari karai", label: "Poosari karai" },
-      ],
-    },
-  ];
-
   const handleFormSubmit = async (formData) => {
     try {
       const res = await api.post("/api/members/", formData);
@@ -77,7 +15,6 @@ function NewMember() {
       }
     } catch (error) {
     } finally {
-      setLoading(false);
     }
   };
 
@@ -93,7 +30,7 @@ function NewMember() {
         }}
       >
         <Form
-          fields={fields}
+          fields={formFields}
           onSubmit={(formData) => handleFormSubmit(formData)}
         />
       </Paper>
